55b0c7004c404ca75f2050bf8a867979580002e8
m0nKey <dva.8a7.super@gmail.com>
2024-09-04 15:03:08 +0700

    Nộp BT05

---
+++ BT05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using BT05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 24";
+            ViewBag.Thang = "Tháng 12";
+            ViewData["Nam"] = "Năm 2004 ";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var TheLoai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Thám"
+            };
+            return View(TheLoai);
+        }
+    }
+}
