385c97436ee901321131307a2bcde3e742a89c51
m0nKey <dva.8a7.super@gmail.com>
2024-09-04 15:38:55 +0700

    Nộp BTVN ví dụ phép nhân chia cộng trừ

---
+++ BTVN3/Controllers/MayTinhController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN3.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult  MayTinh(double a, double b, string pheptinh)
+        {
+            double ketqua = 0;
+            switch (pheptinh)
+            {
+                case "cong":
+                    ketqua = a + b;
+                    break;
+                case "tru":
+                    ketqua = a - b;
+                    break;
+                case "nhan":
+                    ketqua = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                    {
+                        ketqua = a / b;
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Không thể chia cho 0";
+                    }
+                    break;
+                default:
+                    ViewBag.Error = "Phép tính không hợp lệ";
+                    break;
+            }
+
+            ViewBag.KetQua = ketqua;
+            return View();
+        }
+    }
+}
