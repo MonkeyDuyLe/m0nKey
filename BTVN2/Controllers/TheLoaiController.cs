376e10ce1af44f6ab79ab81997344ccad9857986
m0nKey <dva.8a7.super@gmail.com>
2024-09-04 15:29:23 +0700

    Nộp BTVN ví dụ 2 về ViewBag

---
+++ BTVN2/Controllers/TheLoaiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN2.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Lê Thành Duy";
+            ViewBag.MSSV = "1822040141";
+            ViewData["Nam"] = "Năm 2004 ";
+            return View();
+       
+        }   
+    }
+}
