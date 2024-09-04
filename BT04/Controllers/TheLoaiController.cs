3d92efd2b2f1ce5166a29152a0eeb2c3c653cf42
m0nKey <dva.8a7.super@gmail.com>
2024-09-04 14:16:55 +0700

    Nộp BT04

---
+++ BT04/Controllers/TheLoaiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace BT04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            return Ok("id :" + id);
+        }
+        public IActionResult Detail(int id, string ten)
+        {
+            //return Content("id :" + id+"Ten :"+ten);
+            return Content(String.Format("id: {0}; ten: {1}",id ,ten)) ;
+        }
+        public IActionResult Show(List<string>categories)
+        {
+            string content = "Danh sách thể loại";
+            foreach(var category in categories)
+            {
+                content = content + " " + category + ",";
+            }    
+            return Content(content);
+        }
+    }
+}
