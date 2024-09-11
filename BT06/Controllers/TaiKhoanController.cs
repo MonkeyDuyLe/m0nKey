ded24b4514b61bd2cee637edeacab4828a02eee1
m0nKey <dva.8a7.super@gmail.com>
2024-09-11 15:20:36 +0700

    nop bai tap 6

---
+++ BT06/Controllers/TaiKhoanController.cs
@@ -0,0 +1,25 @@
+﻿using BT06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+
+        {
+            {
+                if (taiKhoan != null && taiKhoan.PassWord != null && taiKhoan.PassWord.Length > 0)
+                {
+                    taiKhoan.PassWord = taiKhoan.PassWord + "_da_ma_hoa";
+                }
+                return View(taiKhoan);
+            }
+            return View(taiKhoan);
+        }
+    }
+}
---
+++ BT06/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BT06
+{
+    public class TaiKhoanViewModel
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
