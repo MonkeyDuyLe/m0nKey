61fcf0f5c96d20e4b218509b157ee55a7d2364ba
m0nKey <dva.8a7.super@gmail.com>
2024-09-18 14:43:55 +0700

    Nộp BT07

---
+++ BT07/Models/TheLoaiController.cs
@@ -0,0 +1,38 @@
+﻿using BT07.Data;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace BT07.Models
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai =_db.TheLoai.ToList();
+            ViewBag.theloai=theloai;
+            return View();
+        }
+        [HttpGet]      
+        public IActionResult Create()
+		{
+			
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(TheLoai theLoai)
+		{
+            //Thêm thông tin vào bảng thể loại
+            _db.TheLoai.Add(theLoai);
+            //Lưu lại
+            _db.SaveChanges();
+
+			return View();
+		}
+	}
+}
