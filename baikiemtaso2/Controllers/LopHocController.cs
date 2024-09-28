c7237af868933485a318609a28db4cb5d6e69525
m0nKey <dva.8a7.super@gmail.com>
2024-09-28 15:16:19 +0700

    Nộp bài kiểm tra số 2

---
+++ baikiemtaso2/Controllers/LopHocController.cs
@@ -0,0 +1,104 @@
+﻿using baikiemtaso2.Data;
+using baikiemtaso2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baikiemtaso2.Controllers
+{
+	public class LopHocController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public LopHocController(ApplicationDbContext db)
+		{
+
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			var lophoc = _db.LopHoc.ToList();
+			ViewBag.lophoc = lophoc;
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Create()
+		{
+
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(LopHoc lopHoc)
+		{
+			if (ModelState.IsValid)
+			{
+
+				//Thêm thông tin vào bảng thể loại
+				_db.LopHoc.Add(lopHoc);
+				//Lưu lại
+				_db.SaveChanges();
+
+				return RedirectToAction("Index");
+
+			}
+			return View();
+
+		}
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			if (id == 0)
+			{
+				return NotFound();
+			}
+			var theloai = _db.LopHoc.Find(id);
+			return View(theloai);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(LopHoc lophoc)
+		{
+			if (ModelState.IsValid)
+			{
+				// Thêm thông tin vào bảng TheLoai
+				_db.LopHoc.Update(lophoc);
+				// Lưu lại
+				_db.SaveChanges();
+				// Chuyển trang về index
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			if (id == 0)
+			{
+				return NotFound();
+			}
+			var theloai = _db.LopHoc.Find(id);
+			return View(theloai);
+		}
+
+		[HttpPost]
+		public IActionResult DeleteConfirm(int id)
+		{
+			var theloai = _db.LopHoc.Find(id);
+			if (theloai == null)
+			{
+				return NotFound();
+			}
+			_db.LopHoc.Remove(theloai);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public IActionResult Detail(int id)
+		{
+			var theloai = _db.LopHoc.Find(id);
+			if (theloai == null)
+			{
+				return NotFound();
+			}
+
+			return View();
+		}
+	}
+}
---
+++ baikiemtaso2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using baikiemtaso2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace baikiemtaso2.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<LopHoc> LopHoc { get; set; }
+	}
+}
